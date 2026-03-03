26a92a2995be28d94353c811f31a7e5689920e13
Meraj Ansari <meraj6316@gmail.com>
2026-03-03 16:26:23 +0530

    feat(task-form): integrate reactive form for task input

--- src/app/components/task-form/task-form.ts
+++ src/app/components/task-form/task-form.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './task-form.html',
   styleUrl: './task-form.css',
 })
 export class TaskFormComponent {
+  taskForm = new FormGroup({
+    task: new FormControl('', { nonNullable: true }),
+    status: new FormControl('Todo', {
+      nonNullable: true,
+    }),
+  });
 
+  onSubmit() {
+    // this method is called when form is submitted
+
+    console.log(this.taskForm.getRawValue()); //log values
+
+    this.taskForm.reset(); //to reset the form
+  }
 }
