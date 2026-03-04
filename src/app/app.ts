966763ebb6938b3295941c40231c21773048249f
Meraj Ansari <meraj6316@gmail.com>
2026-03-04 15:59:01 +0530

    feat(task-list): add three task-list components and assign tasks to individual lists

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { TaskFormComponent } from './components/task-form/task-form';
+import { TaskList } from './components/task-list/task-list';
+import { TaskService } from './services/task';
 
 @Component({
   selector: 'app-root',
-  imports: [TaskFormComponent],
+  imports: [TaskFormComponent, TaskList],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  taskService = inject(TaskService);
+  private tasks = this.taskService.tasks;
+
+  todoItems = computed(() => {
+    const tasks = this.tasks();
+    return tasks.filter((t) => t.status === 'Todo');
+  });
+
+  inProgressItems = computed(() => {
+    const tasks = this.tasks();
+    return tasks.filter((t) => t.status === 'InProgress');
+  });
+
+  completedItems = computed(() => {
+    const tasks = this.tasks();
+    return tasks.filter((t) => t.status === 'Completed');
+  });
+}
